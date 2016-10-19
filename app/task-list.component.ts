9eac627ecd4038b0daa6f7a88bc31c53e635740c
andres garciar <andresgarciar96@gmail.com>
2016-10-19 09:42:24 -0400

    finish to-do

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
 
+
 @Component({
   selector: 'task-list',
   template: `
@@ -9,8 +10,8 @@ import { Task } from './task.model';
       <option value="isDone">Show Done</option>
       <option value="notDone" selected="selected">Show Not Done</option>
     </select>
-    <div *ngFor="let currentTask of childTaskList | completeness">
-      <h3>{{ currentTask.description }}</h3>
+    <div *ngFor="let currentTask of childTaskList | completeness:selectedCompleteness">
+      <task-display [task]="currentTask"></task-display>
       <button (click)="editButtonHasBeenClicked(currentTask)">Edit</button>
     </div>
   `
@@ -19,6 +20,11 @@ import { Task } from './task.model';
 export class TaskListComponent {
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
+  public selectedCompleteness: string = "notDone";
+  onChange(optionFromMenu) {
+    this.selectedCompleteness = optionFromMenu;
+    console.log(this.selectedCompleteness);
+  }
   editButtonHasBeenClicked(taskToEdit: Task) {
     this.clickSender.emit(taskToEdit);
   }
